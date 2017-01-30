30f7e51065fe6be17840fd6f7d3ab59069375a89
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-01-30 11:58:08 -0600

    minor

--- misc/datasets.py
+++ misc/datasets.py
@@ -106,8 +106,8 @@ class Dataset(object):
                     e_mean = np.mean(e_sample, axis=0)
                     sampled_embeddings.append(e_mean)
             sampled_embeddings_array = np.array(sampled_embeddings)
-            return np.squeeze(sampled_embeddings_array), sampled_captions
-            # return sampled_embeddings_array, sampled_captions
+            # return np.squeeze(sampled_embeddings_array), sampled_captions
+            return sampled_embeddings_array, sampled_captions
 
     def next_batch(self, batch_size, window):
         """Return the next `batch_size` examples from this data set."""
