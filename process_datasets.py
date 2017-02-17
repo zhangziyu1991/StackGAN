dc53087c28b72649bcd7406c41e15d6d0afa733d
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-02-17 16:15:20 -0600

    test on 251

--- process_datasets.py
+++ process_datasets.py
@@ -7,7 +7,7 @@ import pandas as pd
 BIRD_DIR = 'Data/birds'
 
 def load_filenames(data_dir):
-    filepath = data_dir + '/filenames_flying_7969.pickle'
+    filepath = data_dir + '/filenames_flying_251.pickle'
     with open(filepath, 'rb') as f:
         filenames = pickle.load(f)
     print('Load filenames from: %s (%d)' % (filepath, len(filenames)))
@@ -62,7 +62,7 @@ def convert_birds_dataset_pickle(inpath, set):
         fn = train_filenames[i]
 
         bbox = filename_bbox[fn]
-        mat = scipy.io.loadmat('sketches/{}.mat'.format(fn))
+        mat = scipy.io.loadmat('sketches_flying/{}.mat'.format(fn))
         sketch = mat['sketch']
         sketch = custom_crop(sketch, bbox)
         sketch = scipy.misc.imresize(sketch, [76, 76], 'bicubic').astype('float32')
@@ -71,9 +71,9 @@ def convert_birds_dataset_pickle(inpath, set):
         sketches.append(sketch)
         ids.append(np.int64(fn[:3]))
 
-    print('Saving to {}'.format('Data/birds/{}/sketches_flying_7969.pickle'.format(set)))
-    pickle.dump(sketches, open('Data/birds/{}/sketches_flying_7969.pickle'.format(set), 'wb'))
-    pickle.dump(ids, open('Data/birds/{}/class_info_flying_7969.pickle'.format(set), 'wb'))
+    print('Saving to {}'.format('Data/birds/{}/sketches_flying_251.pickle'.format(set)))
+    pickle.dump(sketches, open('Data/birds/{}/sketches_flying_251.pickle'.format(set), 'wb'))
+    pickle.dump(ids, open('Data/birds/{}/class_info_flying_251.pickle'.format(set), 'wb'))
 
 # def generate_pickle_filenames(text_file):
 #     with open(text_file, "r") as f:
@@ -84,5 +84,5 @@ def convert_birds_dataset_pickle(inpath, set):
 
 if __name__ == '__main__':
     # generate_pickle_filenames('Data/birds/train/filenames_flying_886.txt')
-    convert_birds_dataset_pickle(BIRD_DIR, 'train')
+    convert_birds_dataset_pickle(BIRD_DIR, 'test')
     # convert_birds_dataset_pickle(BIRD_DIR, 'test')
