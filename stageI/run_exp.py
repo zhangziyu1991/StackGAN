15dd35283976be5c231f17ed84180af83e9bd790
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-02-19 19:17:46 -0600

    train with smaller

--- stageI/run_exp.py
+++ stageI/run_exp.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     dataset = TextDataset(datadir, cfg.EMBEDDING_TYPE, 1)
     filename_test = '{}/test'.format(datadir)
     dataset.test = dataset.get_data(filename_test, 251, aug_flag=True)
-    dataset.test2 = dataset.get_data(filename_test, 251, aug_flag=True, animated='_animated')
+    # dataset.test2 = dataset.get_data(filename_test, 251, aug_flag=True, animated='_animated')
     if cfg.TRAIN.FLAG:
         filename_train = '{}/train'.format(datadir)
         dataset.train = dataset.get_data(filename_train, 886, aug_flag=True)
